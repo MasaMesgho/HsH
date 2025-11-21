a52548cc00e4fb6cee120c5abab2e1feb5adb3bd
MasaMesgho <james_sam_whittingham@outlook.com>
2025-11-21 21:14:03 +0000

    basic player physics
    added basic player physics

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -1,17 +1,21 @@
+using UnityEditor.UI;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
 
+    private Rigidbody playerRigidbody;
+    private float moveForce = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerRigidbody.AddRelativeForce(Vector3.forward * moveForce);
     }
 }
