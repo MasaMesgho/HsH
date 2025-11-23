a65ce0a81f4893172df3bb03d2391dd94ac7ecd9
MasaMesgho <james_sam_whittingham@outlook.com>
2025-11-23 17:17:25 +0000

    final bugfixs

--- Assets/Scene_Changer.cs
+++ Assets/Scene_Changer.cs
@@ -7,6 +7,7 @@ public class Scene_Changer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Cursor.lockState = CursorLockMode.None;
 
     }
 
--- Assets/controll.cs
+++ Assets/controll.cs
@@ -6,7 +6,8 @@ public class controll : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Invoke(nameof(sceneChange), 13.5f);
+        Invoke(nameof(sceneChange), 13.5f); 
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
